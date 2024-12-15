77bf4c7b078454184670a58100badd863bf8ca02
Josue Landaverde <129899297+jlndvr@users.noreply.github.com>
2024-12-14 19:09:08 -0600

    Create struct.h

---
+++ struct.h
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+string user;
+int points;   //Data structure 
+string time;
+Node* prev;
+Node* next;
+
+Node(string u, int p, string t){
+user=u;
+points=p;
+time=t;
+prev=nullptr;
+next=nullptr;
+};
+
+};
