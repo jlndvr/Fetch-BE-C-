d1322e6f1f3a8786b5d2b516d914a10a7831576b
Josue Landaverde <129899297+jlndvr@users.noreply.github.com>
2024-12-14 19:08:47 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "struct.cpp"
+using namespace std;
+
+
+//WRITTEN IN C++ USING A DOUBLY LINKED LIST
+
+
+//TEST SOLUTIONS
+void simulateAPI(){
+    Transactions transactions;
+transactions.add("DANNON", 300, "2022-10-31T10:00:00Z");
+transactions.add("UNILEVER", 200, "2022-10-31T11:00:00Z");
+transactions.add("DANNON", -200, "2022-10-31T15:00:00Z");
+transactions.add("MILLER COORS", 10000, "2022-11-01T14:00:00Z");
+transactions.add("DANNON", 1000, "2022-11-02T14:00:00Z");
+
+transactions.spend(transactions,5000);
+
+cout<<"DANNON:"<<transactions.getbalance("DANNON")<<endl;
+cout<<"UNILEVER:"<<transactions.getbalance("UNILEVER")<<endl;
+cout<<"MILLER COORS:"<<transactions.getbalance("MILLER COORS");
+
+}
+
+int main()
+{
+ simulateAPI();
+};
